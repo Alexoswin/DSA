406bf31bb4144b58856c185f3a35d56a77b16d50
oswin <oswinalex1@gmail.com>
2024-01-05 23:17:11 +0530

    2D array

--- array.java
+++ array.java
@@ -6,7 +6,7 @@ public class array {
         System.out.println("------- Arrays -------");
         Scanner input = new Scanner(System.in);
         System.out.println(" press 1 to create a Array");
-        System.out.println("Insert 2 to create a 2D array");
+        System.out.println("Insert 2 to create a 2D Array");
          
         byte inputs = input.nextByte();
 
@@ -30,7 +30,40 @@ public class array {
         
         System.out.println("------- finished -------");
         }
+        else if (inputs ==2){
+            
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("enter the rows for the 2D array ");
+            byte rows = sc2.nextByte();
+            Scanner sc3 = new Scanner(System.in);
+            System.out.println("enter the colums for the 2D array");
+            byte columns = sc3.nextByte();
 
+            int [][] B = new int[rows][columns];
+
+            for(int i = 0;i<rows ; i++){
+
+                for(int j=0 ;j<columns ; j++){
+
+                    Scanner sc4 = new Scanner(System.in);
+                    System.out.println("enter the element for index " + i + ", " + j);
+                    B[i][j] = sc4.nextInt();
+                }
+            }
+
+             for(int i = 0;i<rows; i++){
+
+                for(int j=0 ;j< columns; j++){
+
+                    System.out.print(B[i][j]+ " ");
+                }
+                System.out.println();
+
+            }
+            System.out.println("-------finished-------");
+
+               
+        }
         else{
             System.out.println("press a valid number");
         }
