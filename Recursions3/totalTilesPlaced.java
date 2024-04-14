56a852b37b57b8bfd3572619922881cf3810efbd
oswin <oswinalex1@gmail.com>
2024-04-14 22:18:07 +0530

    tiles combinations in a nxm dimension room

---
+++ Recursions3/totalTilesPlaced.java
@@ -0,0 +1,24 @@
+public class  totalTilesPlaced{
+
+    public static int calculate(int n , int m){
+
+        if(n ==m){
+            return 2;
+
+        }
+        if(n<m){
+            return 1;
+        }
+
+        int vertical = calculate(n -m , m);
+        int horizontal = calculate(n -1 , m );
+        return vertical +horizontal;
+    }
+
+    public static void main (String[]args){
+
+
+        System.out.println("Total combinations of tiles placed in a nxm room");
+        System.out.println(calculate(4,2));
+    }
+}
