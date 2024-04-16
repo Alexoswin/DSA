e7027c213c69e2c1151580e0d078f39ab2110483
oswin <oswinalex1@gmail.com>
2024-04-16 18:09:53 +0530

    permutation in a string

---
+++ backtrackingRecursionPermutation/recursion/permutation/Backtracking.java
@@ -0,0 +1,28 @@
+public class Backtracking{
+
+    public static void calculate( String str , String str2, int idx){
+
+
+        if( str.length() == 0){
+
+            System.out.println(str2);
+            return;
+        }
+        
+
+
+        for(int i = 0 ; i<str.length(); i++){
+            char current = str.charAt(i);
+
+            String replace = str.substring(0,i)+ str.substring(i+1);
+              
+            calculate(replace,str2 + current, idx + 1) ;
+        }
+    }
+
+    public static void main (String[]args){
+
+        String  str = "abc";
+        calculate(str,"",0);
+    }
+}
