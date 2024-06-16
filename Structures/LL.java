affbdf127609335a1a2ad008dcedfdbd4f65dd95
Alexoswim <oswinalex1@gmail.com>
2024-06-16 16:01:14 +0530

    linked list

---
+++ Structures/LL.java
@@ -0,0 +1,75 @@
+class LL{
+
+Node head;
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data){
+
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+// ADD  - FIRST AND LAST 
+    public void addFirst(String data){
+        Node newNode  = new Node(data);
+
+        if(head == null){
+
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data){
+
+        Node newNode  = new Node(data);
+
+        if(head == null){
+
+            head = newNode;
+            return;
+        }
+
+        Node curNode = head;
+
+        while(curNode.next != null){
+
+            curNode = curNode.next;
+        }
+
+        curNode.next = newNode;
+    }
+     public void printList(){
+        
+       if(head == null){
+        System.out.println("list is empty");
+       }
+        Node curNode = head;
+
+        while(curNode != null){
+            System.out.print(curNode.data + " -->");
+            curNode = curNode.next;
+        }
+
+        
+     }
+
+    public static void  main (String args[]){
+
+        LL list = new LL();
+
+        list.addFirst("abc");
+        list.addFirst("Def");
+        list.addLast("xyz");
+        list.printList();
+
+
+
+    }
+  
+}
