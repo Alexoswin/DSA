dea4d37c18b46ef9c16613f7eeb1401512174ff0
Alexoswim <oswinalex1@gmail.com>
2024-06-27 00:43:29 +0530

    linked list package in java

---
+++ Structures/linlist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class linlist {
+    public static void main (String []args){
+        System.out.println("linked list package");
+
+        LinkedList<String> List = new LinkedList<>();
+
+        List.addFirst("Adsa");
+        List.addLast("Dasda");
+        List.addFirst("sda");
+
+        System.out.println(List.element());
+
+
+    }
+}
