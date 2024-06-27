2b8e3dc48099b4a927e6d52fd57a2a81076647bc
Alexoswim <oswinalex1@gmail.com>
2024-06-27 18:00:33 +0530

    array list and linklist package in java

--- Structures/linlist.java
+++ Structures/linlist.java
@@ -8,8 +8,21 @@ public class linlist {
         List.addFirst("Adsa");
         List.addLast("Dasda");
         List.addFirst("sda");
+        System.out.println(List.size());
+        System.out.println(List.get(2));
+        
 
-        System.out.println(List.element());
+        //Array list 
+        ArrayList<String> ll = new ArrayList<>();
+        ll.add("Asd");
+        ll.add("sda");
+        ll.add("dsad");
+
+        System.out.println(ll.size());
+        ll.set(0, "dsad");
+        
+        System.out.println(ll.get(2));
+
 
 
     }
