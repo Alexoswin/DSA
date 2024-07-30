024625d17eeba085e8dd0fcb192542da8bff1ada
oswin <oswinalex1@gmail.com>
2024-07-30 23:21:54 +0530

    queue in java

---
+++ Structures/queue/bqueue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class bqueue{
+
+    public static void main(String []args){
+
+            Queue<Integer> q = new LinkedList<>();
+
+            q.add(1);
+            q.add(2);
+            q.add(3);
+
+            q.remove();
+            System.out.println(q.peek());
+            q.add(4);
+
+            while(!q.isEmpty()){
+                System.out.println(q.peek());
+                q.remove();
+            }
+
+    
+    }
+}
