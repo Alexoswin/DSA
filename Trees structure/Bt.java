5c1347dcad6a2dd6c215e3315795c52d9a878ada
Alexoswim <oswinalex1@gmail.com>
2024-08-15 20:04:34 +0530

    b Trees

---
+++ Trees structure/Bt.java
@@ -0,0 +1,44 @@
+ class Bt {
+
+    static class Node{
+
+        int data ; 
+        Node left ;
+        Node right ;
+
+        Node(int data ){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+  static class Btree{
+
+        static int idx = -1;
+
+        public static Node BuildTree(int  nodes []){
+
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+            
+            return newNode;
+
+        }
+    }
+     public static void main (String [] args ){
+
+        System.out.println("--- Binarry tree ---");
+
+
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Btree tree = new Btree();
+        Node root =  tree.BuildTree(nodes);
+        System.out.println(root.data);
+     }
+}
