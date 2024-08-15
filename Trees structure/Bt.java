7fa6d885b82c32daf0d5081a796fb5ece877d2f9
Alexoswim <oswinalex1@gmail.com>
2024-08-15 23:38:20 +0530

    in order, pre order and post order  tree search

--- Trees structure/Bt.java
+++ Trees structure/Bt.java
@@ -30,6 +30,44 @@
             return newNode;
 
         }
+
+
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+
+            System.out.println(root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root){
+            if(root == null){
+                return;
+            }
+
+         
+            inorder(root.left);
+            System.out.println(root.data);
+            inorder(root.right);
+        }
+
+
+        public static void postorder(Node root){
+            if(root == null){
+                return;
+            }
+
+         
+            postorder(root.left);
+            
+            postorder(root.right);
+            System.out.println(root.data);
+        }
+
+
+
     }
      public static void main (String [] args ){
 
@@ -40,5 +78,18 @@
         Btree tree = new Btree();
         Node root =  tree.BuildTree(nodes);
         System.out.println(root.data);
+
+        tree.preorder(root);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        tree.inorder(root);
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        tree.postorder(root);
+
      }
 }
