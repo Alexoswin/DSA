fc785d5649f73ce5a0719ae73660225a2c96e1cd
Alexoswin <oswinalex1@gmail.com>
2025-02-10 16:29:24 +0530

    graph using adjusancy list

--- Graphs/graph.java
+++ Graphs/graph.java
@@ -1,45 +1,63 @@
 import java.util.*;
-public class graph{
-    // class to store edges sorce source and destination
-    class Edge{
 
-        int s;
-        int d;
+public class  graph{
+    
+ static class  edge{
 
-        Edge(int s, int d){
-            this.s = s ;
-            this.d = d;
+        int src;
+        int dest;
+
+        edge(int src , int dest){
+            this.src = src;
+            this.dest = dest;
+
         }
     }
 
-    public void createGraph(ArrayList<Edge> graph[]){
-        
+
+    public static void createGraph(ArrayList<edge> graph[]){
+
 
         for(int i = 0 ; i< graph.length ; i++){
 
-            graph[i] = new ArrayList<Edge>();
+            graph[i] = new ArrayList<edge>();
         }
 
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 3));
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        graph[0].add(new edge(0,1));
+        graph[0].add(new edge(0,2));
 
-        graph[2].add(new Edge(2, 3));
+
+        graph[1].add(new edge(1,2));
+        graph[1].add(new edge(1,3));
+
+        graph[2].add(new edge(2,3));
+        graph[2].add(new edge(2,1));
         
-        graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
 
+        graph[3].add(new edge(3,2));
+        graph[3].add(new edge(3,1));
     }
-    public static void main (String [] args ){
-        System.out.print(" graph using adjencicy list");
-
-        ArrayList<Edge> graph[] = new ArrayList<>[4 ] ;
 
 
+    public static void main (String [] args){
+        System.out.println("Graphs using adjuscent list");
+
+        ArrayList<edge> graph[] = new ArrayList[4];
+        createGraph(graph);
+
+        // print neighbours of each node 
+        for(int i = 0 ; i< graph.length ; i++){
+         
         
+            System.out.print("node: " + i + ": ");
+            
+            for(int j = 0 ; j< graph[i].size(); j++){
+
+                System.out.println("src: " + graph[i].get(j).src + " -> " + "Dest: " + graph[i].get(j).dest);
+            }
+            System.out.println();
+        }
 
     }
 }
